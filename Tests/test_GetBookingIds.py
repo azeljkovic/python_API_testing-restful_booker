d6303a933cfd4abac4f9d5e56ca9ecb064891542
unknown <aleksandar.zeljkovic91@gmail.com>
2020-04-05 12:34:51 +0200

    added tests for nonexisting names and last names

--- Tests/test_GetBookingIds.py
+++ Tests/test_GetBookingIds.py
@@ -2,7 +2,6 @@ import requests
 import json
 import endpoints
 import pytest
-import re
 
 
 def testGetAllIDs():
@@ -12,35 +11,60 @@ def testGetAllIDs():
     expectedIDs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     IDs = getIDsFromResponse(response)
 
-    assert r.status_code == requests.codes.ok #check the status code
-    assert sorted(IDs) == sorted(expectedIDs) #check if all booking IDs are returned
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert sorted(IDs) == sorted(expectedIDs)  # check if all booking IDs are returned
 
 
 def testGetIdsByFirstName():
     payload = {'firstname': 'Susan'}
     r = requests.get(endpoints.Booking, params=payload)
     response = r.json()
-    
-    expectedIDs = [1, 7]
+
+    first_id = response[0].get("bookingid")
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert type(first_id) == int  # check if id exists in response
+
+
+def testGetIdsByNonExistingFirstName():
+    payload = {'firstname': 'vcxvcx'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    expectedIDs = []
     IDs = getIDsFromResponse(response)
 
-    assert r.status_code == requests.codes.ok #check the status code
-    assert sorted(IDs) == sorted(expectedIDs) #check if all booking IDs are returned
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert sorted(IDs) == sorted(expectedIDs)  # check if all booking IDs are returned
+
 
 def testGetIdsByLastName():
     payload = {'lastname': 'Jackson'}
     r = requests.get(endpoints.Booking, params=payload)
     response = r.json()
     
-    expectedIDs = [1, 8, 10]
-    IDs = getIDsFromResponse(response)
+    first_id = response[0].get("bookingid")
 
     assert r.status_code == requests.codes.ok #check the status code
-    assert sorted(IDs) == sorted(expectedIDs) #check if all booking IDs are returned
+    assert type(first_id) == int  # check if id exists in response
+
+
+def testGetIdsByNonExistingLastName():
+    payload = {'lastname': 'hfjhgjg'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    expectedIDs = []
+    IDs = getIDsFromResponse(response)
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert sorted(IDs) == sorted(expectedIDs)  # check if all booking IDs are returned
+
 
 '''
 helper functions below
 '''
+
 
 def getIDsFromResponse(resp):
     id_array = []
@@ -49,5 +73,3 @@ def getIDsFromResponse(resp):
         id_array.append(key.get("bookingid"))
         
     return id_array
-
-
