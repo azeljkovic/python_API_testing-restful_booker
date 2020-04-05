6abfcc91ebaf6c24118dcbfc3712b78b6618ac21
Aleksandar <aleksandar.zeljkovic91@gmail.com>
2020-04-05 15:43:51 +0200

    added checkin/checkout date tests

--- Tests/test_GetBookingIds.py
+++ Tests/test_GetBookingIds.py
@@ -1,7 +1,5 @@
 import requests
-import json
 import endpoints
-import pytest
 
 
 def testGetAllIDs():
@@ -35,17 +33,17 @@ def testGetIdsByNonExistingFirstName():
     IDs = getIDsFromResponse(response)
 
     assert r.status_code == requests.codes.ok  # check the status code
-    assert sorted(IDs) == sorted(expectedIDs)  # check if all booking IDs are returned
+    assert sorted(IDs) == sorted(expectedIDs)  # check if response is empty
 
 
 def testGetIdsByLastName():
     payload = {'lastname': 'Jackson'}
     r = requests.get(endpoints.Booking, params=payload)
     response = r.json()
-    
+
     first_id = response[0].get("bookingid")
 
-    assert r.status_code == requests.codes.ok #check the status code
+    assert r.status_code == requests.codes.ok  # check the status code
     assert type(first_id) == int  # check if id exists in response
 
 
@@ -58,7 +56,70 @@ def testGetIdsByNonExistingLastName():
     IDs = getIDsFromResponse(response)
 
     assert r.status_code == requests.codes.ok  # check the status code
-    assert sorted(IDs) == sorted(expectedIDs)  # check if all booking IDs are returned
+    assert sorted(IDs) == sorted(expectedIDs)  # check if response is empty
+
+
+def testGetIdsByCheckinDate():
+    payload = {'checkin': '2016-09-21'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    first_id = response[0].get("bookingid")
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert type(first_id) == int  # check if id exists in response
+
+
+def testGetIdsByNonExistingCheckinDate():
+    payload = {'checkin': '2222-09-21'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    expectedIDs = []
+    IDs = getIDsFromResponse(response)
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert sorted(IDs) == sorted(expectedIDs)  # check if response is empty
+
+
+def testGetIdsByInvalidFormatCheckinDate():
+    payload = {'checkin': '27.3.1943.'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert response == "Some valid error message"  # check if appropriate message ia provided
+
+
+def testGetIdsByCheckoutDate():
+    payload = {'checkout': '2020-03-23'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    first_id = response[0].get("bookingid")
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert type(first_id) == int  # check if id exists in response
+
+
+def testGetIdsByNonExistingCheckoutDate():
+    payload = {'checkout': '2222-09-21'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    expectedIDs = []
+    IDs = getIDsFromResponse(response)
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert sorted(IDs) == sorted(expectedIDs)  # check if response is empty
+
+def testGetIdsByInvalidFormatCheckoutDate():
+    payload = {'checkout': '27.3.1943.'}
+    r = requests.get(endpoints.Booking, params=payload)
+    response = r.json()
+
+    assert r.status_code == requests.codes.ok  # check the status code
+    assert response == "Some valid error message"  # check if appropriate message ia provided
 
 
 '''
@@ -71,5 +132,5 @@ def getIDsFromResponse(resp):
 
     for key in resp:
         id_array.append(key.get("bookingid"))
-        
+
     return id_array
